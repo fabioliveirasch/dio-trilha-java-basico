28358e9816003f0cca7405970a3035be9797b3c7
fabioliveirasch <fabioscholiv@gmail.com>
2023-03-13 08:16:47 -0300

    Atualizando aulas

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,9 +1,28 @@
+import java.io.Console;
 
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		int n = 1;
+		int x = 0;
+		int i;
+		
+		while (n < 10) {
+			if (n % 2 != 0) {
+				for (i = 3; i * i <= n; i += 2) {
+					if(n % i == 0)
+						break;
+				}
+				if(i < n) {
+					x++;
+				}
+			}
+			n++;
+		}
+		System.out.println(x);
+		
+		
 	}
 
 }
